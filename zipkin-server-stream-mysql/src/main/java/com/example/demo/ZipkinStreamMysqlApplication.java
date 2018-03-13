7fd13941b8cf0251e083d0d701cd3e2740447fcc
just-moth <1073717529@qq.cpm>
2018-03-13 16:03:33 +0800

    commit初体验

---
+++ zipkin-server-stream-mysql/src/main/java/com/example/demo/ZipkinStreamMysqlApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+/**
+ * @author： lxh
+ * @description：
+ * @created: 16:01 2018/1/20
+ * @modified by:
+ */
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class ZipkinStreamMysqlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinStreamMysqlApplication.class, args);
+    }
+}
